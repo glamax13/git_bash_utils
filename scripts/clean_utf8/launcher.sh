405e1542dfde2b6940f861e9f98e0a93de53fc4c
Quentin JAMELOT <quentin.jamelot@groupe-salaun.com>
2020-01-02 17:39:58 +0100

    Making worker process multiple lines to minimize file descriptor read time

--- scripts/clean_utf8/launcher.sh
+++ scripts/clean_utf8/launcher.sh
@@ -29,40 +29,61 @@ do
     esac
 done
 ###Setup
+#Function
+function output {
+    wait
+    for task in "${arr_task[@]}"
+    do
+        printf %s\\n "$(cat <&$task)" >> "$to/$file"
+    done
+    for fd in $(ls /proc/$$/fd/)
+    do
+        [ $fd -gt 2 ] && exec {fd}<&-
+    done
+    arr_task=()
+}
+
 #Comparisson arrays
 make_arr "$script_folder/src/lst_clean_sort.txt"; clean+=( "${MAKE_ARR_RET[@]}" )
 make_arr "$script_folder/src/lst_dirty_sort.txt"; dirty+=( "${MAKE_ARR_RET[@]}" )
 max_comp=${#clean[@]}
 arr_task=()
+arr_line=()
 echo "Les mentions {rep} seront à remplacer manuellements."
 
+###Work
 if [ $ext_file ]
 then
     echo "Processing $ext_file files..."
     find_foi -e "$ext_file" -w "$from" -t "f"
     for file in ${ARR_FOI[@]}
-    do 
-        nb_line=0
-        while IFS= read -r line || [ -n "$line" ]
-        do
-            exec {fd}< <(echo $(worker "$line"))
-            arr_task+="$fd "
-            nb_line=$(($nb_line+1))
+    do
+        nb_line="$(wc -l $file)"
+        nb_line=${nb_line/$file/}
+        if [ $nb_line -gt 999 ]
+        then
+            while IFS= read -r line || [ -n "$line" ]
+            do
+                arr_line+=("$line")
+                if [ $((${#arr_line[@]} % 999)) -eq 0 ]
+                then
+                    exec {fd}< <(echo "$(worker "$arr_line")")
+                    arr_task+=("$fd")
+                    arr_line=()
 
-            if [[ $(($nb_line % 999)) -eq 0 ]]
-            then
-                wait
-                for task in $arr_task
-                do
-                    printf %s\\n "$(cat <&$task)" >> "$to/$file"
-                done
-                for fd in $(ls /proc/$$/fd/)
-                do
-                    [ $fd -gt 2 ] && exec {fd}<&-
-                done
-                arr_task=()
-            fi
-        done < "$from/$file"
+                    if [ ${#arr_task[@]} -gt 13 ]
+                    then
+                        output
+                    fi
+                fi
+            done < "$from/$file"
+            output
+        else
+            while IFS= read -r line || [ -n "$line" ]
+            do
+                worker "$line" >> "$to/$file"
+            done < "$from/$file"
+        fi
     done
 else
     echo "No ext provided"
--- scripts/clean_utf8/worker.sh
+++ scripts/clean_utf8/worker.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 function worker {
     i=0
-    ori=$1
-    swap=$1
-
-    while [[ $i -lt $max_comp ]]
+    arr_output=()
+    while [ $i -lt ${#arr_line[@]} ]
     do
-        selector="${dirty[$i]}"
-        replacer="${clean[$i]}"
-        swap="${swap//$selector/$replacer}"
+        line=${arr_line[$i]}
         i=$(($i+1))
-    done
-    selector="{a_tilda_maj}"
-    replacer="Ã"
-    swap="${swap//$selector/$replacer}"
-    if [ "$ori" != "$swap" ]
-    then
-		echo -e "$nb_line	$file\n$ori\n$swap" >> "$logs/double_utf8_changes.log"
-        if [[ "$swap" =~ "{rep}" ]]
+        ori=$line
+        swap=$line
+    
+        j=0
+        while [[ $j -lt $max_comp ]]
+        do
+            selector="${dirty[$j]}"
+            replacer="${clean[$j]}"
+            swap="${swap//$selector/$replacer}"
+            j=$(($j+1))
+        done
+        selector="{a_tilda_maj}"
+        replacer="Ã"
+        swap="${swap//$selector/$replacer}"
+        if [ "$ori" != "$swap" ]
         then
-			echo -e "$nb_line	$file\n$ori\n$swap" >> "$logs/double_utf8_to_change.log"
+            echo -e "$nb_line	$file\n$ori\n$swap" >> "$logs/double_utf8_changes.log"
+            if [[ "$swap" =~ "{rep}" ]]
+            then
+                echo -e "$nb_line	$file\n$ori\n$swap" >> "$logs/double_utf8_to_change.log"
+            fi
         fi
-    fi
-    printf %s\\n "$swap"
+        echo "$swap"
+    done
 }
